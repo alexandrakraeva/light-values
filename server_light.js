6da218487ae4aa3bfdd114d3f58f23250aa4853d
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2024-02-20 13:01:08 +0100

    as

--- server_light.js
+++ server_light.js
@@ -4,12 +4,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
+// Array to store the light values
+let lightValues = [];
+
 app.post('/send-light-value', (req, res) => {
     const lightValue = req.body.lightValue;
     console.log(`Received light value: ${lightValue}`);
-    // Here you can add the logic to send the value to Firebase
+    // Store the received light value
+    lightValues.push(lightValue);
 
     res.status(200).send('Light value received');
+});
+
+// GET route to display the stored light values
+app.get('/light-values', (req, res) => {
+    res.status(200).json(lightValues);
 });
 
 app.listen(PORT, () => {
