9b7d62f36ad4ee42aa50a1572eef9305c5cdd629
alexandrakraeva <66311877+alexandrakraeva@users.noreply.github.com>
2024-02-20 13:59:14 +0100

    _____

--- server_light.js
+++ server_light.js
@@ -14,12 +14,11 @@ const db = admin.firestore();
 // Array to store the light values
 let lightValues = [];
 
-app.post('/send-light-value', (req, res) => {
+app.post('/send-light-value', async (req, res) => {
     const lightValue = req.body.lightValue;
     console.log(`Received light value: ${lightValue}`);
-    // Store the received light value
-    lightValues.push(lightValue);
 
+    // Store the received light value in Firestore
     const docRef = db.collection('lightValues').doc(); // Creates a new document in the 'lightValues' collection
     await docRef.set({ value: lightValue, timestamp: admin.firestore.FieldValue.serverTimestamp() });
 
@@ -28,9 +27,7 @@ app.post('/send-light-value', (req, res) => {
 
 
 // GET route to display the stored light values
-app.get('/light-values', (req, res) => {
-    res.status(200).json(lightValues);
-
+app.get('/light-values', async (req, res) => {
     const lightValues = [];
     const snapshot = await db.collection('lightValues').get();
     snapshot.forEach(doc => {
@@ -39,20 +36,18 @@ app.get('/light-values', (req, res) => {
     res.status(200).json(lightValues);
 });
 
+
 // Route to handle GET requests to the root URL path
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
 // Route to handle GET requests to update the Lux value
-app.get('/updateLux', (req, res) => {
-    const lux = req.query.lux; // Extract Lux value from query parameters
+app.get('/updateLux', async (req, res) => {
+    const lux = req.query.lux;
     console.log(`Received Lux value: ${lux}`);
-    // Store the received Lux value
-    lightValues.push(lux);
 
-    res.status(200).send('Lux value received');
-
+    // Store the received Lux value in Firestore
     const docRef = db.collection('luxValues').doc(); // You can choose to store in the same or a different collection
     await docRef.set({ value: lux, timestamp: admin.firestore.FieldValue.serverTimestamp() });
 
